eed8269cafd73986a5755e5ec19edefa6461d0c5
elkksmedina <salethsofiasalcedohostia@gmail.com>
2026-04-08 00:54:34 -0500

    DetalleCarrito

--- src/main/java/killa/modelo/DetalleCarrito.java
+++ src/main/java/killa/modelo/DetalleCarrito.java
@@ -3,14 +3,37 @@ package killa.modelo;
 public class DetalleCarrito {
     private int idDetalleCarrito;
     private int cantidad;
-//navegación
     private Producto producto;
-    private Carro carro;
-// getters setters
-    public int getCantidad() { return cantidad; }
-    public void setCantidad(int cantidad) { this.cantidad = cantidad; }
 
-    public Producto getProducto() { return producto; }
-    public void setProducto(Producto producto) { this.producto = producto; }
+
+    public DetalleCarrito() {}
+
+    public int getIdDetalleCarrito() {
+        return idDetalleCarrito;
+    }
+
+    public void setIdDetalleCarrito(int idDetalleCarrito) {
+        this.idDetalleCarrito = idDetalleCarrito;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    public double calcularSubtotal() {
+        return 0.0;
+    }
 }
 
