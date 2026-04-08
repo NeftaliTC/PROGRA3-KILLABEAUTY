e8fd7aca11ad674cabd1e54cc2e4e83a805c1f5c
Alejandra Prado <alepradovaldivia@gmail.com>
2026-04-08 00:33:33 -0500

    Update Carro.java

--- src/main/java/killa/modelo/Carro.java
+++ src/main/java/killa/modelo/Carro.java
@@ -2,9 +2,10 @@ package killa.modelo;
 import java.util.Date;
 import java.util.List;
 import java.util.ArrayList;
+
 public class Carro {
 
-    private int id_carrito;
+    private int idCarrito;
     private Date fechaDeCreacion;
     private String estado; // con un enum tambien
 
@@ -14,5 +15,30 @@ public class Carro {
 
     public Carro() {
         this.items = new ArrayList<>();
+    }
+        
+    public int getId_carrito() {
+        return id_carrito; 
+    }
+    public Date getFechaDeCreacion() { 
+        return fechaDeCreacion; 
+    }
+    public EstadoCarrito getEstado() {
+        return estado;
+    }
+    public Cliente getCliente() {
+        return cliente;
+    }
+    public void setId_carrito(int id_carrito) {
+        this.id_carrito = id_carrito;
+    }
+    public void setFechaDeCreacion(Date fechaDeCreacion) {
+        this.fechaDeCreacion = fechaDeCreacion;
+    }
+    public void setEstado(EstadoCarrito estado) {
+        this.estado = estado; 
+    }
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente; 
     }
 }
