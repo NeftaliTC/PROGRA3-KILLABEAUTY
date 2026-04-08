356ed604f9c24552b31b73ebfb537e7ee859297f
fabiana <162488688+FabianaAguilar@users.noreply.github.com>
2026-04-08 00:29:04 -0500

    Update DetalleCarrito.java

--- src/main/java/killa/modelo/DetalleCarrito.java
+++ src/main/java/killa/modelo/DetalleCarrito.java
@@ -4,15 +4,11 @@ public class DetalleCarrito {
 //navegación
     private Producto producto;
     private Carro carro;
-
+// getters setters
     public int getCantidad() { return cantidad; }
     public void setCantidad(int cantidad) { this.cantidad = cantidad; }
 
     public Producto getProducto() { return producto; }
     public void setProducto(Producto producto) { this.producto = producto; }
-
-    public String toString() {
-        return producto.getNombre() + " x" + cantidad;
-    }
 }
 
