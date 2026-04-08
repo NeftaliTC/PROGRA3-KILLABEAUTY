423f72efcac62fef7a0e673da5a40b4d57a53f95
Cielo Espino Lopez <cieloespino022@gmail.com>
2026-04-08 01:06:22 -0500

    Update Marca.java

--- src/main/java/killa/modelo/Marca.java
+++ src/main/java/killa/modelo/Marca.java
@@ -4,9 +4,18 @@ public class Marca {
     private int idMarca;
     private String descripcion;
     private String paisDeOrigen;
-    
+
+    //Constructores
     public Marca() {};
 
+    public Marca(int idMarca, String nombre, String descripcion, boolean activo) {
+        this.idMarca = idMarca;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.activo = activo;
+    }
+    
+    //Getters y setters
     public int getIdMarca() {
         return idMarca;
     }
