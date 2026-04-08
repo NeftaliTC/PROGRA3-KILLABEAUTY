a55f5cf35eae9248c1a06a5c8b5f3065b9a45d57
Cielo Espino Lopez <cieloespino022@gmail.com>
2026-04-08 00:45:36 -0500

    Update Descuento.java

--- src/main/java/killa/modelo/Descuento.java
+++ src/main/java/killa/modelo/Descuento.java
@@ -1,10 +1,62 @@
 package killa.modelo;
 import java.util.Date;
 public class Descuento {
-    private int id_descuento;
+    private int idDescuento;
     private String descripcion;
     private double porcentajeDeDescuento;
     private Date fechaInicio;
     private Date fechaFin;
+
+    //Constructores
     public Descuento() {}
+
+    public Descuento(int idDescuento, String descripcion, double porcentajeDeDescuento) {
+        this.idDescuento = idDescuento;
+        this.descripcion = descripcion;
+        this.porcentajeDeDescuento = porcentajeDeDescuento;
+    }
+
+    //Metodos
+    public void validarVigencia() {}
+    
+    //Getters y setters
+    public int getIdDescuento() {
+        return idDescuento;
+    }
+
+    public void setIdDescuento(int idDescuento) {
+        this.idDescuento = idDescuento;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPorcentajeDeDescuento() {
+        return porcentajeDeDescuento;
+    }
+
+    public void setPorcentajeDeDescuento(double porcentajeDeDescuento) {
+        this.porcentajeDeDescuento = porcentajeDeDescuento;
+    }
+
+    public Date getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(Date fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public Date getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(Date fechaFin) {
+        this.fechaFin = fechaFin;
+    }
 }
