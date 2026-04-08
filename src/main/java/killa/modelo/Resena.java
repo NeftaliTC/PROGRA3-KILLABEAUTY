859f1812dcde00e972ebff9e9146ee945982ae05
neftali <a20233391@pucp.edu.pe>
2026-04-07 23:24:45 -0500

    Capa modelo -> primeras clasesitas

--- src/main/java/killa/modelo/Resena.java
+++ src/main/java/killa/modelo/Resena.java
@@ -1,4 +1,16 @@
 package killa.modelo;
-
+import java.util.Date;
 public class Resena {
+    private int id_resena;
+    private String comentario;
+    private int calificacion;
+    private boolean verificado;
+    private Date fechaDePublicacion;
+
+    // NAVEGABILIDAD: La reseña debe conocer a quién la escribió y sobre qué producto
+    private Cliente cliente;
+    private Producto producto;
+
+    public Resena() {
+    }
 }
