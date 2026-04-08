f549ac91ae244fff6bb2faa249d9a4c882c62d9d
neftali <a20233391@pucp.edu.pe>
2026-04-07 23:18:13 -0500

    Capa modelo -> primeras clasesitas

--- src/main/java/killa/modelo/Cupon.java
+++ src/main/java/killa/modelo/Cupon.java
@@ -1,4 +1,14 @@
 package killa.modelo;
-
+import java.util.Date;
 public class Cupon {
+
+    private int id_cupon;
+    private String codigo;
+    private String descripcion;
+    private double porcentajeDeDescuento;
+    private Date fecha_inicio;
+    private Date fecha_fin;
+    private boolean activo;
+
+    public Cupon() {}
 }
