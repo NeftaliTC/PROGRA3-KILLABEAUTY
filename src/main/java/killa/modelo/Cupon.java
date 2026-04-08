4d786c8015193b11ead60cc2794d951199030c89
Cielo Espino Lopez <cieloespino022@gmail.com>
2026-04-08 00:34:26 -0500

    Update Cupon.java

--- src/main/java/killa/modelo/Cupon.java
+++ src/main/java/killa/modelo/Cupon.java
@@ -2,13 +2,82 @@ package killa.modelo;
 import java.util.Date;
 public class Cupon {
 
-    private int id_cupon;
+    private int idCupon;
     private String codigo;
     private String descripcion;
     private double porcentajeDeDescuento;
-    private Date fecha_inicio;
-    private Date fecha_fin;
+    private Date fechaInicio;
+    private Date fechaFin;
     private boolean activo;
 
+    //Constructores
     public Cupon() {}
+    
+    public Cupon(int idCupon, String codigo, double porcentajeDeDescuento) {
+        this.idCupon = idCupon;
+        this.codigo = codigo;
+        this.porcentajeDeDescuento = porcentajeDeDescuento;
+    }
+
+    //Metodos
+    public void validarVigencia() {}
+    public void calcularDescuentoCupon(double subtotal) {} //Calcula el monto a descontar
+    
+
+    //Getters y setters
+    public int getIdCupon() {
+        return idCupon;
+    }
+
+    public void setIdCupon(int idCupon) {
+        this.idCupon = idCupon;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPorcentajeDeDescuento() {
+        return porcentajeDeDescuento;
+    }
+
+    public void setPorcentajeDeDescuento(double porcentajeDeDescuento) {
+        this.porcentajeDeDescuento = porcentajeDeDescuento;
+    }
+
+    public Date getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(Date fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public Date getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(Date fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
 }
