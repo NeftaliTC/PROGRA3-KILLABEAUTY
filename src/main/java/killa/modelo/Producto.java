f83ee214fb11eb33e8a081f907db389f4e41ed7a
Alejandra Prado <alepradovaldivia@gmail.com>
2026-04-08 00:46:39 -0500

    Update Producto.java

--- src/main/java/killa/modelo/Producto.java
+++ src/main/java/killa/modelo/Producto.java
@@ -23,4 +23,36 @@ public class Producto {
 
     public void registrarProducto() {}
     public void actualizarStock(int cantidad) {}
+
+    public int getIdProducto() {
+        return idProducto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getPrecioLista() {
+        return precioLista;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setIdProducto(int idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setPrecioLista(double precioLista) {
+        this.precioLista = precioLista;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }    
 }
