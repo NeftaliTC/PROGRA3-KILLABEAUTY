0aa4e1326be54210e96e1b4f366e2cfb94efbe6e
fabiana <162488688+FabianaAguilar@users.noreply.github.com>
2026-04-08 00:09:38 -0500

    Update Direccion.java

--- src/main/java/killa/modelo/Direccion.java
+++ src/main/java/killa/modelo/Direccion.java
@@ -1,11 +1,12 @@
 package killa.modelo;
 public class Direccion {
-    private int id;
+    private int id_direccion;
     private String calle;
     private String numero;
     private String distrito;
     private String departamento;
     private String codigoPostal;
     private String referencia;
+    
     public Direccion(){};
 }
