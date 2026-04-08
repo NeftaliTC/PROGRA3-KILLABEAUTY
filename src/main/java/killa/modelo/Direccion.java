63fe7c51106a6557b91192d272282ada468bcad7
fabiana <162488688+FabianaAguilar@users.noreply.github.com>
2026-04-07 23:50:18 -0500

    Create Direccion.java

---
+++ src/main/java/killa/modelo/Direccion.java
@@ -0,0 +1,10 @@
+public class Direccion {
+    private int id;
+    private String calle;
+    private String numero;
+    private String distrito;
+    private String departamento;
+    private String pais;
+    private String codigoPostal;
+    private String referencia;
+}
