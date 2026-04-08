c277046639eed62f62030aa795f1e46ec60f46df
Alejandra Prado <alepradovaldivia@gmail.com>
2026-04-08 00:41:33 -0500

    Update Marca.java

--- src/main/java/killa/modelo/Marca.java
+++ src/main/java/killa/modelo/Marca.java
@@ -4,4 +4,31 @@ public class Marca {
     private int idMarca;
     private String descripcion;
     private String paisDeOrigen;
+    
+    public Marca() {};
+
+    public int getIdMarca() {
+        return idMarca;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public String getPaisDeOrigen() {
+        return paisDeOrigen;
+    }
+
+    public void setIdMarca(int idMarca) {
+        this.idMarca = idMarca;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public void setPaisDeOrigen(String paisDeOrigen) {
+        this.paisDeOrigen = paisDeOrigen;
+    }
+    
 }
