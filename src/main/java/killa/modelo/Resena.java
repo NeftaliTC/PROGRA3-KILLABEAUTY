1b932abf86c511989d2439367bb7a95398e06b44
elkksmedina <salethsofiasalcedohostia@gmail.com>
2026-04-08 00:41:41 -0500

    reseña

--- src/main/java/killa/modelo/Resena.java
+++ src/main/java/killa/modelo/Resena.java
@@ -1,16 +1,72 @@
 package killa.modelo;
 import java.util.Date;
 public class Resena {
-    private int id_resena;
+    private int idResena;
     private String comentario;
     private int calificacion;
     private boolean verificado;
-    private Date fechaDePublicacion;
+    private Date fechaPublicacion;
 
-    // NAVEGABILIDAD: La reseña debe conocer a quién la escribió y sobre qué producto
     private Cliente cliente;
     private Producto producto;
 
-    public Resena() {
+    public Resena() {}
+
+    public int getIdResena() {
+        return idResena;
     }
+
+    public void setIdResena(int idResena) {
+        this.idResena = idResena;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+    public int getCalificacion() {
+        return calificacion;
+    }
+
+    public void setCalificacion(int calificacion) {
+        this.calificacion = calificacion;
+    }
+
+    public boolean isVerificado() {
+        return verificado;
+    }
+
+    public void setVerificado(boolean verificado) {
+        this.verificado = verificado;
+    }
+
+    public Date getFechaPublicacion() {
+        return fechaPublicacion;
+    }
+
+    public void setFechaPublicacion(Date fechaPublicacion) {
+        this.fechaPublicacion = fechaPublicacion;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    public void validarCalificacion() {}
 }
