e877e7f5dd192607b7d69bf1262aac715d7d0b49
neftali <a20233391@pucp.edu.pe>
2026-04-19 20:43:58 -0500

    Estructura para conectar la bd ya hecha, solo completar el archivo .properties de dbManager

--- dbManager/src/main/java/pe/edu/pucp/dbManager/Main.java
+++ dbManager/src/main/java/pe/edu/pucp/dbManager/Main.java
@@ -1,9 +1,29 @@
 package pe.edu.pucp.dbManager;
 
-
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Iniciando prueba");
 
+        try {
+
+            dbManager manager = dbManager.getInstance();
+
+
+            Connection con = manager.getConnection();
+
+            if (con != null) {
+                System.out.println("==========================================");
+                System.out.println("   CONEXIÓN EXITOSA");
+                System.out.println("==========================================");
+                con.close();
+            }
+        } catch (SQLException e) {
+            System.out.println("Error al cerrar la conexión: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("ERROR DE CONEXIÓN: " + e.getMessage());
+        }
+    }
 }
-
-
