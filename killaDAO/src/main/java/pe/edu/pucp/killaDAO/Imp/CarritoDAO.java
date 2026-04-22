7792c4797e6a9b14c060da550395f9c8028f35ab
arom12321 <alepradovaldivia@gmail.com>
2026-04-21 19:38:56 -0500

    nombre

--- killaDAO/src/main/java/pe/edu/pucp/killaDAO/Imp/CarritoDAO.java
+++ killaDAO/src/main/java/pe/edu/pucp/killaDAO/Imp/CarritoDAO.java
@@ -1,7 +1,11 @@
 package pe.edu.pucp.killaDAO.Imp;
 
+import pe.edu.pucp.killaBeauty.killaModelo.Carrito;
 import pe.edu.pucp.killaDAO.Base.BaseDAO;
 
-public interface CarritoDAO extends BaseDAO {
+import java.sql.SQLException;
+import java.util.List;
 
+public interface CarritoDAO extends BaseDAO<Carrito,Integer> {
+    List<Carrito> lsitAll() throws SQLException;
 }
