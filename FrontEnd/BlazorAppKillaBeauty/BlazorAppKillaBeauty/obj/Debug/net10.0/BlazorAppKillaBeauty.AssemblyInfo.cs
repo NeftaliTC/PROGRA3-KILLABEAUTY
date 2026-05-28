c2c0a1c10b1fdfef6faa0abb385d92380b06e6ec
AnApple-afk <a20212667@pucp.edu.pe>
2026-05-27 21:59:14 -0500

    Implementación de interfaz de inicio de sesión
    - Se creó la página de inicio de sesión de Killa Beauty.
    - Se implementó el diseño visual del formulario login.
    - Se añadieron estilos CSS para inputs, botón e iconografía.
    - Se integró navegación desde el navbar hacia la pantalla Login.

--- FrontEnd/BlazorAppKillaBeauty/BlazorAppKillaBeauty/obj/Debug/net10.0/BlazorAppKillaBeauty.AssemblyInfo.cs
+++ FrontEnd/BlazorAppKillaBeauty/BlazorAppKillaBeauty/obj/Debug/net10.0/BlazorAppKillaBeauty.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorAppKillaBeauty")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2cf441b1c485aa9dc4dafc63505a5d264561255")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e2ad0308b448576bc8c62d1a776338290b131cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorAppKillaBeauty")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorAppKillaBeauty")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
