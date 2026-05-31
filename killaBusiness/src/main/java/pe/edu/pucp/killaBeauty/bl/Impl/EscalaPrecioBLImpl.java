f126a90f0f4c832d88844ce99b26a63720cc2d99
Cielo <cieloespino022@gmail.com>
2026-05-31 14:25:27 -0500

    refactor: Implementación de Permiso con eliminación lógica y validación de Dirección

--- killaBusiness/src/main/java/pe/edu/pucp/killaBeauty/bl/Impl/EscalaPrecioBLImpl.java
+++ killaBusiness/src/main/java/pe/edu/pucp/killaBeauty/bl/Impl/EscalaPrecioBLImpl.java
@@ -1,4 +1,4 @@
-package pe.edu.pucp.killaBeauty.bl.impl;
+package pe.edu.pucp.killaBeauty.bl.Impl;
 
 import pe.edu.pucp.killaBeauty.bl.EscalaPrecioBL;
 import pe.edu.pucp.killaBeauty.bl.exception.BusinessLogicException;
