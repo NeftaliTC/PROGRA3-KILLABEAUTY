a3176ac368436f16de634c0f8478e5432b490771
elkksmedina <salethsofiasalcedohostia@gmail.com>
2026-05-31 00:54:02 -0500

    actualizacion SubCategoriaDAO y DAOImpl

--- killaDAO/src/main/java/pe/edu/pucp/killaDAO/SubCategoriaDAO.java
+++ killaDAO/src/main/java/pe/edu/pucp/killaDAO/SubCategoriaDAO.java
@@ -9,8 +9,5 @@ import java.util.List;
 public interface SubCategoriaDAO extends BaseDAO<Subcategoria, Integer> {
     List<Subcategoria> listAll() throws SQLException;
 
-    // Métodos extra necesarios por FK id_categoria
     List<Subcategoria> listByCategoriaId(Integer idCategoria) throws SQLException;
-    Subcategoria save(Subcategoria subcategoria, Integer idCategoria) throws SQLException;
-    Subcategoria update(Subcategoria subcategoria, Integer idCategoria) throws SQLException;
 }
