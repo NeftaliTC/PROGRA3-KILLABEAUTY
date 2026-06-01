1c7c07dcb7f356b5e3c2f3bb061bb88723e73a78
elkksmedina <salethsofiasalcedohostia@gmail.com>
2026-06-01 03:38:50 -0500

    actualizacion Model

--- killaModelo/src/main/java/pe/edu/pucp/killaBeauty/killaModelo/Pedido.java
+++ killaModelo/src/main/java/pe/edu/pucp/killaBeauty/killaModelo/Pedido.java
@@ -1,5 +1,7 @@
 package pe.edu.pucp.killaBeauty.killaModelo;
 
+import pe.edu.pucp.killaBeauty.killaModelo.Promocionales.Cupon;
+
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
