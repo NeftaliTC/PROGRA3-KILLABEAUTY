747194f1da563a18c6ccf9eda3818577d04f015a
AnApple-afk <a20212667@pucp.edu.pe>
2026-06-15 21:15:28 -0500

    Configurar proyecto REST con Jakarta EE y GlassFish
    - Configuración inicial del módulo killa-rs
    - Integración de Jakarta EE y JAX-RS
    - Configuración de GlassFish para despliegue local
    - Generación del artifact war exploded

---
+++ Backend/killa-rs/src/main/java/pe/edu/pucp/killars/killars/RestApplication.java
@@ -0,0 +1,9 @@
+package pe.edu.pucp.killars.killars;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("services")
+public class RestApplication extends Application {
+// Esta clase activa el escaneo de recursos REST
+}
