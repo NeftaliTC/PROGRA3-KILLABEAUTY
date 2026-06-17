8f692a67ed2f0c6e8d4dcbc831731608d4e97ec2
Sapineft <a20233391@pucp.edu.pe>
2026-06-17 04:00:16 -0500

    feat: Agregar clases model de marca y pais

---
+++ FrontEnd/BlazorAppKillaBeauty/BlazorAppKillaBeauty/ClienteREST/Models/Marca.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorAppKillaBeauty.ClienteREST.Models
+{
+    public class Marca
+    {
+        public int Id { get; set; }
+        public string Descripcion { get; set; } = "";
+
+        // Ahora Pais usa el enum que acabamos de crear arriba
+        public Pais Pais { get; set; }
+
+        public bool Activo { get; set; }
+    }
+}
---
+++ FrontEnd/BlazorAppKillaBeauty/BlazorAppKillaBeauty/ClienteREST/Models/Pais.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BlazorAppKillaBeauty.ClienteREST.Models
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum Pais
+    {
+        EEUU = 1,
+        COREA = 2,
+        JAPON = 3,
+        PERU = 4,
+        CHILE = 5,
+        MEXICO = 6,
+        COLOMBIA = 7,
+        CHINA = 8
+    }
+}
