74e850fe453a6286fdc4fcf02e77dd6a026a36d6
Sapineft <a20233391@pucp.edu.pe>
2026-06-16 22:54:42 -0500

    feat(killarest): Agregar cascarones a los services

--- KillaREST/src/main/java/pe/edu/pucp/killabeauty/killarest/services/usuarios/PermisoRS.java
+++ KillaREST/src/main/java/pe/edu/pucp/killabeauty/killarest/services/usuarios/PermisoRS.java
@@ -1,4 +1,23 @@
 package pe.edu.pucp.killabeauty.killarest.services.usuarios;
 
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import pe.edu.pucp.killaBeauty.bl.Impl.PermisoBLImpl;
+import pe.edu.pucp.killaBeauty.bl.Impl.Promocionales.CuponBLImpl;
+import pe.edu.pucp.killaBeauty.bl.PermisoBL;
+import pe.edu.pucp.killaBeauty.bl.Promocionales.CuponBL;
+
+@Path("/cupones")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class PermisoRS {
+    private final PermisoBL permisoBL = new PermisoBLImpl();
+    @GET
+    public Response listarPermisos() {
+        return Response.ok().build();
+    }
 }
--- KillaREST/src/main/java/pe/edu/pucp/killabeauty/killarest/services/usuarios/UsuarioRS.java
+++ KillaREST/src/main/java/pe/edu/pucp/killabeauty/killarest/services/usuarios/UsuarioRS.java
@@ -1,4 +1,23 @@
 package pe.edu.pucp.killabeauty.killarest.services.usuarios;
 
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import pe.edu.pucp.killaBeauty.bl.Impl.Promocionales.CuponBLImpl;
+import pe.edu.pucp.killaBeauty.bl.Impl.UsuarioBLImpl;
+import pe.edu.pucp.killaBeauty.bl.Promocionales.CuponBL;
+import pe.edu.pucp.killaBeauty.bl.UsuarioBL;
+
+@Path("/usuarios")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class UsuarioRS {
+    private final UsuarioBL usuarioBL = new UsuarioBLImpl();
+    @GET
+    public Response listarCupones() {
+        return Response.ok().build();
+    }
 }
