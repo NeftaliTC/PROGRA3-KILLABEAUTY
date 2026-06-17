8f3b59a47ff3dcdcfa763ac752d6d74bd41a934a
Sapineft <a20233391@pucp.edu.pe>
2026-06-16 21:45:54 -0500

    build: agregar nuevo modulo killaREST e integrarlo al pom padre

---
+++ KillaREST/src/main/java/pe/edu/pucp/killabeauty/killarest/KillaRestApplication.java
@@ -0,0 +1,8 @@
+package pe.edu.pucp.killabeauty.killarest;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class KillaRestApplication extends Application {
+}
