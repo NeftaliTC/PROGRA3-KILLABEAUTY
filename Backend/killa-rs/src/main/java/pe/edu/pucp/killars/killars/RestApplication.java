40fa271f46aca36efa79f4c7b97afb887f5869a7
AnApple-afk <a20212667@pucp.edu.pe>
2026-06-16 22:06:25 -0500

    Eliminar avance de rest

--- Backend/killa-rs/src/main/java/pe/edu/pucp/killars/killars/RestApplication.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.pucp.killars.killars;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("WebApplication")
-public class RestApplication extends Application {
-// Esta clase activa el escaneo de recursos REST
-}
