e18fe3ce5f339704e824ddae59b30ae8a1e9f7a1
neftali <a20233391@pucp.edu.pe>
2026-06-23 19:18:02 -0500

    fix(utils): mejorar lectura de excepciones en HttpClientUtils

--- FrontEnd/BlazorAppKillaBeauty/BlazorAppKillaBeauty/ClienteREST/Utils/HttpClientUtils.cs
+++ FrontEnd/BlazorAppKillaBeauty/BlazorAppKillaBeauty/ClienteREST/Utils/HttpClientUtils.cs
@@ -69,7 +69,14 @@
                 // Muy parecido al POST, pero usando PutAsJsonAsync
                 HttpResponseMessage response = await _httpClient.PutAsJsonAsync(url, data, _jsonOptions);
 
-                response.EnsureSuccessStatusCode();
+                if (!response.IsSuccessStatusCode)
+                {
+                    // Leemos el texto que Java nos envió de regreso (ej. "El correo ya existe")
+                    string errorDeJava = await response.Content.ReadAsStringAsync();
+
+                    // Lanzamos el error pero CON EL TEXTO REAL
+                    throw new Exception(errorDeJava);
+                }
 
                 return await response.Content.ReadFromJsonAsync<T>(_jsonOptions);
             }
