c78a3144a3abacb6d9ddc8f53c38ec7478d0c595
AnApple-afk <a20212667@pucp.edu.pe>
2026-06-27 23:22:00 -0500

    feat(checkout): agregar simulación de validación de compra y validaciones de datos
    - Se agregó una simulación de validación de pago con porcentaje de aprobación/rechazo.
    - Se implementó un modal de "Validando compra" antes de finalizar el pedido.
    - Se agregó un modal para informar cuando la validación del pago es rechazada.
    - Se mejoraron las validaciones del formulario de checkout (DNI, RUC y teléfono).
    - Se incorporaron validaciones de autenticación para el acceso al proceso de compra.

--- FrontEnd/BlazorAppKillaBeauty/BlazorAppKillaBeauty/Services/AuthService.cs
+++ FrontEnd/BlazorAppKillaBeauty/BlazorAppKillaBeauty/Services/AuthService.cs
@@ -1,3 +1,5 @@
+using System.Net;
+
 public class AuthService
 {
     public bool EstaLogueado { get; private set; }
@@ -5,6 +7,8 @@ public class AuthService
     public string NombreUsuario { get; private set; } = "";
     public string CorreoElectronico { get; private set; } = "";
     public string Rol { get; private set; } = "";
+
+    public string Dni { get; private set; } = "";
 
     //public void LoginCliente(string nombre, int usuarioId = 2)
     //{
@@ -29,12 +33,13 @@ public class AuthService
     //    NombreUsuario = nombre;
     //    Rol = "SuperAdmin";
     //}
-    public void Login(string nombre, int usuarioId, string rol, string correoElectronico)
+    public void Login(string nombre, int usuarioId, string rol, string correoElectronico, string dni)
     {
         EstaLogueado = true;
         UsuarioId = usuarioId;
         NombreUsuario = nombre;
         CorreoElectronico = correoElectronico;
+        Dni = dni;
         Rol = rol ?? "Cliente";  // si llega null se le asigna cliente 
     }
     public void Logout()
@@ -43,6 +48,7 @@ public class AuthService
         UsuarioId = null;
         NombreUsuario = "";
         CorreoElectronico = "";
+        Dni = "";
         Rol = "";
     }
 }
